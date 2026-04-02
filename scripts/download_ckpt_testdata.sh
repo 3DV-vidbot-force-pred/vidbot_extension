1fca8db97bcc2c265bf030e4d1f0acc077af0305
David Korčák <korcadav@fel.cvut.cz>
2026-04-02 16:33:20 +0200

    remove nonexistent dataset file from download script

--- scripts/download_ckpt_testdata.sh
+++ scripts/download_ckpt_testdata.sh
@@ -1,7 +1,4 @@
 # mkdir -p datasets
-gdown https://drive.google.com/uc?id=1IDCJ-wB05sMVKdLiG0IO_OajsY0ihvpi
-unzip vidbot_data_demo.zip -d datasets/
-rm vidbot_data_demo.zip
 
 gdown https://drive.google.com/uc?id=14ByEGX4zKB7VjIE7fybXiq11kpRvvMqz
 unzip epickitchens_traj_demo.zip -d datasets/
